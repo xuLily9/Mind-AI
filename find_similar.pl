6753ab66c24240692de8734f179f68a23c594c28
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2023-09-08 12:51:46 +0100

    update, caculating the similarity considering synonyms

--- find_similar.pl
+++ find_similar.pl
@@ -1,9 +1,12 @@
-% database
+% Database with sentences and their synonyms
 sentence("That is a sunny day").
 sentence("That is a happy dog").
-sentence("That is a very happy person").
+sentence("That is a very cheerful person").
 
-% Calculate the similarity (number of identical words) between two sentences
+% Define synonyms
+synonym(happy, cheerful).
+
+% Calculate the similarity between two sentences considering synonyms
 similarity(Sentence1, Sentence2, Similarity) :-
     tokenize_atom(Sentence1, Tokens1),
     tokenize_atom(Sentence2, Tokens2),
@@ -15,7 +18,20 @@ similarity(Sentence1, Sentence2, Similarity) :-
 
 % Find the database sentence that is most similar to the input sentence
 most_similar(InputSentence, MostSimilarSentence) :-
-    findall(Similarity-Sentence, (sentence(Sentence), similarity(InputSentence, Sentence, Similarity)), Similarities),
+    findall(Similarity-Sentence,
+        (sentence(Sentence), similarity(InputSentence, Sentence, Similarity)),
+        Similarities),
     keysort(Similarities, SortedSimilarities),
     reverse(SortedSimilarities, ReversedSimilarities),
     [MostSimilarity-MostSimilarSentence|_] = ReversedSimilarities.
+
+% Modify the input sentence to include synonyms
+modify_input_sentence(InputSentence, ModifiedSentence) :-
+    tokenize_atom(InputSentence, Tokens),
+    findall(Synonym, (member(Token, Tokens), synonym(Token, Synonym)), Synonyms),
+    append(Tokens, Synonyms, ModifiedTokens),
+    atomic_list_concat(ModifiedTokens, ' ', ModifiedSentence).
+
+% Query with the modified input sentence
+%?- modify_input_sentence("That is a happy person", ModifiedInput),
+ %  most_similar(ModifiedInput, MostSimilar).
