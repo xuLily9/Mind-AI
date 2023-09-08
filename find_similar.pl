f7c1982cdfa5a3205e9485a72014a8d7d03f86ca
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2023-09-08 12:36:49 +0100

    find similar sentence

--- find_similar.pl
+++ find_similar.pl
@@ -1,33 +1,21 @@
-% Define similarity rules based on Jaccard similarity.
-jaccard_similarity(S1, S2, Similarity) :-
-    atomic_list_concat(Words1, ' ', S1),
-    atomic_list_concat(Words2, ' ', S2),
-    intersection(Words1, Words2, Intersection),
-    union(Words1, Words2, Union),
-    length(Intersection, IntLen),
-    length(Union, UnionLen),
-    Similarity is IntLen / UnionLen.
+% database
+sentence("That is a sunny day").
+sentence("That is a happy dog").
+sentence("That is a very happy person").
 
-% Database of sentences.
-sentence('I adore sunny weather').
-sentence('I hate rainy days').
-sentence('Sunny days make me happy').
+% Calculate the similarity (number of identical words) between two sentences
+similarity(Sentence1, Sentence2, Similarity) :-
+    tokenize_atom(Sentence1, Tokens1),
+    tokenize_atom(Sentence2, Tokens2),
+    intersection(Tokens1, Tokens2, CommonTokens),
+    length(CommonTokens, CommonTokenCount),
+    length(Tokens1, TokenCount1),
+    length(Tokens2, TokenCount2),
+    Similarity is CommonTokenCount / max(TokenCount1, TokenCount2).
 
-% Find the most similar sentence.
-find_most_similar(Input, MostSimilar) :-
-    sentence(MostSimilar),  % Initialize MostSimilar with the first sentence.
-    jaccard_similarity(Input, MostSimilar, MaxSimilarity),
-    find_most_similar(Input, MostSimilar, MaxSimilarity).
-
-find_most_similar(_, _, _).
-find_most_similar(Input, CurrentMostSimilar, MaxSimilarity) :-
-    sentence(NextSentence),
-    jaccard_similarity(Input, NextSentence, Similarity),
-    Similarity > MaxSimilarity,
-    find_most_similar(Input, NextSentence, Similarity).
-
-find_most_similar(Input, CurrentMostSimilar, MaxSimilarity) :-
-    sentence(NextSentence),
-    jaccard_similarity(Input, NextSentence, Similarity),
-    Similarity =< MaxSimilarity,
-    find_most_similar(Input, CurrentMostSimilar, MaxSimilarity).
+% Find the database sentence that is most similar to the input sentence
+most_similar(InputSentence, MostSimilarSentence) :-
+    findall(Similarity-Sentence, (sentence(Sentence), similarity(InputSentence, Sentence, Similarity)), Similarities),
+    keysort(Similarities, SortedSimilarities),
+    reverse(SortedSimilarities, ReversedSimilarities),
+    [MostSimilarity-MostSimilarSentence|_] = ReversedSimilarities.
