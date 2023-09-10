c75d5ef9756bb0dcfbf8412f1ac12f0c624c9b10
m09238yx <yifan.xu-9@postgrad.manchester.ac.uk>
2023-09-10 21:11:12 +0100

    update

--- generate_similar3.pl
+++ generate_similar3.pl
@@ -9,33 +9,34 @@ synonyms("to", ["to", "towards", "for", "in the direction of"]).
 synonyms("Paris", ["Paris", "the City of Light", "the French capital"]).
 
 % Replace a word with a randomly selected synonym
+% It takes a Word, list of Synonyms, and binds Synonym to a random synonym that is not equal to the original Word.
 replace_word(Word, Synonyms, Synonym) :-
     member(Synonym, Synonyms),
     Synonym \= Word.
 
 % Generate a sentence with replaced words
 generate_similar_sentence(OriginalSentence, SimilarSentence) :-
-    split_string(OriginalSentence, " ", " ", OriginalWords),
-    maplist(replace_word_in_wordlist, OriginalWords, _Synonyms, SimilarWords),
+    split_string(OriginalSentence, " ", " ", OriginalWords), % This splits the OriginalSentence into a list of words OriginalWords.
+    maplist(replace_word_in_wordlist, OriginalWords, _Synonyms, SimilarWords), %This maps replace_word_in_wordlist over each word in OriginalWords to generate a list of SimilarWords with synonyms replaced.
     atomic_list_concat(SimilarWords, ' ', SimilarSentence).
-
-% Replace a word in a list with a synonym
+ 
+% Replace a word in a list with a synonym， 
 replace_word_in_wordlist(Word, Synonyms, Synonym) :-
     (synonyms(Word, Synonyms) ->
-        random_member(Synonym, Synonyms)
+        random_member(Synonym, Synonyms) %This does the actual word replacement. It looks up the synonyms for the Word. If any are found, it randomly chooses one. Otherwise, it just keeps the original Word.
     ; % If no synonyms are available, keep the original word
         Synonym = Word
     ).
 
 % Generate 20 similar sentences based on a seed sentence
 generate_20_similar_sentences(SeedSentence, SimilarSentences) :-
-    generate_20_similar_sentences_helper(SeedSentence, [], SimilarSentences).
+    generate_20_similar_sentences_helper(SeedSentence, [], SimilarSentences). 
 
 generate_20_similar_sentences_helper(_, SimilarSentences, SimilarSentences) :- length(SimilarSentences, 20).
 generate_20_similar_sentences_helper(SeedSentence, Acc, SimilarSentences) :-
     generate_similar_sentence(SeedSentence, SimilarSentence),
-    \+ member(SimilarSentence, Acc),  % Ensure no duplicates
-    generate_20_similar_sentences_helper(SeedSentence, [SimilarSentence|Acc], SimilarSentences).
+    \+ member(SimilarSentence, Acc),  % Ensure no duplicates. This checks that the new sentence is not already in the accumulator list Acc.
+    generate_20_similar_sentences_helper(SeedSentence, [SimilarSentence|Acc], SimilarSentences). %This recursively adding the new sentence to the accumulator.
 
 % Pretty print a list of sentences
 pretty_print_sentences([]).
